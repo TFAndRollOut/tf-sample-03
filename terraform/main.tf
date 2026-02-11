cc62310ecbf0e5dde8fe34c84c218884bc3ddf42
Kev Haynes <kevhaynes1@hotmail.com>
2026-02-11 10:34:33 +0000

    Initial commit with simple modules

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+module "storage_account" {
+  source = "../modules/storage-account"
+  
+  # Pass any necessary variables to the module
+  storage_account_name = "mystorageaccount"
+  resource_group_name  = "myresourcegroup"
+  location             = "eastus"
+}
+
+module "virtual_network" {
+  source = "../modules/virtual-network"
+  
+  # Pass any necessary variables to the module
+  virtual_network_name = "myvirtualnetwork"
+  resource_group_name  = "myresourcegroup"
+  location             = "eastus"
+}
+
+module "subnet" {
+  source = "../modules/subnet"
+  
+  # Pass any necessary variables to the module
+  subnet_name = "mysubnet"
+  virtual_network_name = "myvirtualnetwork"
+  resource_group_name  = "myresourcegroup"
+  location             = "eastus"
+}
